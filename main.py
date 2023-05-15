dc32c543008ea91a7b0809df161f1d91f474d30b
Lucas Claro <31224118+LucasClaroNicolau@users.noreply.github.com>
2023-05-15 13:16:29 -0300

    OK
    OK

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
-import os
-
+from flask import Flask, render_template, request
+import smtplib
 app = Flask(__name__)
-
+#server = smtplib.SMTP("smtp.gmail.com", 587)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template("index.html")
 
+@app.route('/contato', methods=['GET', 'POST'])
+def contato():
+#    msg = "Nome: " +request.form['name']+ "\nTelefone: "+request.form['phone']+"\nE-mail: "+request.form['email']+"\nMenssagem: "+ request.form['message']
+#    server.sendmail("mercadotrevisol@hotmail.com", "lucas.claro.tr@gmail.com", msg)
+#    print(request.form['name'])
+#    print(request.form['phone'])
+#    print(request.form['email'])
+#    print(request.form['message'])
+    return ""
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(port=80)
+#    server.login("mercadotrevisol@hotmail.com", "g033127g")
